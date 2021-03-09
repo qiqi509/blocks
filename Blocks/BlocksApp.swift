57619de26f109b86efe79ba1e328b357cc45bc10
qiqi <qiqi509@hotmail.com>
2021-03-09 17:21:34 +1300

    Initial commit

---
+++ Blocks/BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksApp.swift
+//  Blocks
+//
+//  Created by apple on 2021/3/9.
+//
+
+import SwiftUI
+
+@main
+struct BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
